1587c11becbed018a150eb9a7d2b68a1fce2c5be
Adriano Rocha <adriano.olr@gmail.com>
2020-09-24 01:27:09 -0300

    Modifica icones de botões para o MaterialIcons, e navegação para tela de editar

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import { View, Image, TouchableOpacity,Text} from 'react-native';
+import { View, Image, TouchableOpacity, Text } from 'react-native';
 
 import backgroundImage from '../../assets/background-detail-page.png';
-import Logo from '../../assets/logo.png'
-import line from '../../assets/Line.png'
+import Logo from '../../assets/logo.png';
+import line from '../../assets/Line.png';
 import styles from './styles';
 
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { MaterialIcons as Icon } from '@expo/vector-icons';
 
 import { useNavigation } from '@react-navigation/native';
 
 const Detail: React.FC = () => {
-
   const navigation = useNavigation();
 
   return (
@@ -22,82 +21,65 @@ const Detail: React.FC = () => {
         resizeMode="stretch"
       />
 
-      <View style = {styles.header}>
-
-        <View style = {styles.hedaerOfheader}>
-
-          <TouchableOpacity style = {styles.iconBack} onPress={() => navigation.navigate('Home')}>
-            <Icon name="long-arrow-left" size={18} color='#FF5D5D'/>
+      <View style={styles.header}>
+        <View style={styles.hedaerOfheader}>
+          <TouchableOpacity
+            style={styles.iconBack}
+            onPress={() => navigation.navigate('Home')}
+          >
+            <Icon name="arrow-back" size={24} color="#FF5D5D" />
           </TouchableOpacity>
 
-          <Image source={Logo} style = {styles.imgLogo}></Image>
+          <Image source={Logo} style={styles.imgLogo}></Image>
 
-          <View style = {styles.icons}>
-            
-            <TouchableOpacity style = {styles.icon}>
-              <Icon name = 'pencil' size = {16} color='#FF5D5D'/>
+          <View style={styles.icons}>
+            <TouchableOpacity
+              style={styles.icon}
+              onPress={() => navigation.navigate('Edit')}
+            >
+              <Icon name="edit" size={24} color="#FF5D5D" />
             </TouchableOpacity>
 
-            <TouchableOpacity style = {styles.icon}>
-              <Icon name = 'trash' size = {16} color='#FF5D5D'/>
+            <TouchableOpacity style={styles.icon}>
+              <Icon name="delete" size={24} color="#FF5D5D" />
             </TouchableOpacity>
-            
           </View>
-          
         </View>
 
-        <View style = {styles.titlePage}>
-            <Text style = {styles.textTile}> TituloCompromisso </Text>
+        <View style={styles.titlePage}>
+          <Text style={styles.textTile}> TituloCompromisso </Text>
         </View>
-        
       </View>
 
-      <View style  = {styles.body}>
-
-        <View style={styles.line}> 
-
-          <Image
-            source={line}
-            style={styles.imgLine}
-            resizeMode="stretch"
-          />
-
-
+      <View style={styles.body}>
+        <View style={styles.line}>
+          <Image source={line} style={styles.imgLine} resizeMode="stretch" />
         </View>
 
-        <View style = {styles.tagTime}>
-
-            <View style = {styles.tag}>
-              <Icon name="circle" size={12} color='#ff5d5d'/>
-              <Text>8:00</Text>
-            </View>
-            
-          
-            <View style = {styles.tag}>
-              <Icon name="circle" size={12} color='#ff5d5d'/>
-              <Text>12:00</Text>
-            </View>
-            
+        <View style={styles.tagTime}>
+          <View style={styles.tag}>
+            <Icon name="lens" size={12} color="#ff5d5d" />
+            <Text>8:00</Text>
           </View>
 
-        <View style = {styles.detail}>
-
-          <View style = {styles.task}>
-
-            <View style = {styles.tagColor}>
-              <Icon name="circle" size={25} color='#45EDF8'/>
-            </View>
-
-            <Text style = {styles.titulo}> Terça-feira das 8:00 às 12:00 </Text>
-
-            <Text style ={styles.descricao} >Descrição do compromissso</Text>
-
+          <View style={styles.tag}>
+            <Icon name="lens" size={12} color="#ff5d5d" />
+            <Text>12:00</Text>
           </View>
-
         </View>
-        
+
+        <View style={styles.detail}>
+          <View style={styles.task}>
+            <View style={styles.tagColor}>
+              <Icon name="lens" size={25} color="#45EDF8" />
+            </View>
+
+            <Text style={styles.titulo}> Terça-feira das 8:00 às 12:00 </Text>
+
+            <Text style={styles.descricao}>Descrição do compromissso</Text>
+          </View>
+        </View>
       </View>
-
     </View>
   );
 };
